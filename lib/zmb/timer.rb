e30f624d1161bf9745c1b24a2e67c6003c34e514
Kyle Fuller <inbox@kylefuller.co.uk>
2010-03-05 00:14:14 +0000

    Support passing data to timers

--- lib/zmb/timer.rb
+++ lib/zmb/timer.rb
@@ -3,17 +3,22 @@ class Timer
   
   attr_accessor :delegate
   
-  def initialize(delegate, symbol, interval, repeat=false) # interval is in seconds (decimals accepted)
+  def initialize(delegate, symbol, interval, repeat=false, data=nil) # interval is in seconds (decimals accepted)
     @delegate = delegate
     @symbol = symbol
     @interval = interval
     @repeat = repeat
+    @data = data
     
     @fire_at = Time.now + interval
   end
 
   def fire(sender)
-    @delegate.send @symbol
+    if @data then
+      @delegate.send @symbol, @data
+    else
+      @delegate.send @symbol
+    end
 
     if not @repeat
       sender.timer_delete(self) if sender.respond_to?('timer_delete')
