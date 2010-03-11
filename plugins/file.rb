3eb15f8bf6aa5d5a43703c075c1dc8667d2a3cfa
Kyle Fuller <inbox@kylefuller.co.uk>
2010-03-11 22:12:58 +0000

    Create a plugin to read/write files

---
+++ plugins/file.rb
@@ -0,0 +1,57 @@
+class FileIO
+  def initialize(sender, s); end
+  
+  def commands
+    {
+      'cat' => [:cat, 1, {
+        :permission => 'admin',
+        :help => 'View a file',
+        :usage => '/home/zynox/file' }],
+      'write' => [:write, 2, {
+        :permission => 'admin',
+        :help => 'Write to a file',
+        :usage => '/home/zynox/hello Hello world!' }],
+      'append' => [:append, 2, {
+        :permission => 'admin',
+        :help => 'Add data to the end of a file',
+        :usage => '/home/zynox/hello Another hello world!' }],
+    }
+  end
+  
+  def cat(e, file)
+    begin
+      File.read(File.expand_path(file))
+    rescue
+      "file not found or access denied"
+    end
+  end
+  
+  def write(e, file, data)
+    begin
+      f = File.open(File.expand_path(file), 'w')
+      f.write(data)
+      'data written'
+    rescue
+      'access denied'
+    ensure
+      f.close unless f.nil?
+    end
+  end
+  
+  def append(e, file, data)
+    begin
+      f = File.open(File.expand_path(file), 'a')
+      f.write(data)
+      'data written to end of file'
+    rescue
+      'access denied'
+    ensure
+      f.close unless f.nil?
+    end
+  end
+end
+
+Plugin.define do
+  name 'file'
+  object FileIO
+end
