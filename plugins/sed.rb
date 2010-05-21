a055c44c8ad0c1ab76de6e127b469922f07ab4b6
Kyle Fuller <inbox@kylefuller.co.uk>
2010-05-21 21:36:45 +0100

    Add a plugin called sed

---
+++ plugins/sed.rb
@@ -0,0 +1,18 @@
+class Sed
+  def initialize(sender, settings)
+    @messages = Hash.new
+  end
+  
+  def event(sender, e)
+    if e.message =~ /s\/(\S+)\/(\S+)\// then
+      e.reply("#{e.name} meant " + @messages[e.userhost].sub($1, $2))
+    else
+      @messages[e.userhost] = e.message if e.message?
+    end
+  end
+end
+
+Plugin.define do
+  name 'sed'
+  object Sed
+end
