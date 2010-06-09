a1d9e77d382b5f154ba7f40feeba023d49e87a48
Kyle Fuller <inbox@kylefuller.co.uk>
2010-06-09 13:27:23 +0100

    Add a bash like !! to the sed plugin

--- plugins/sed.rb
+++ plugins/sed.rb
@@ -3,9 +3,11 @@ class Sed
     @messages = Hash.new
   end
   
-  def event(sender, e)
+  def pre_event(sender, e)
     if e.message =~ /^s\/(\S+)\/(\S+)\/$/ then
       e.reply("#{e.name} meant " + @messages[e.userhost].sub($1, $2))
+    elsif e.message =~ /^!!(.+)/ then
+      e.message = @messages[e.userhost] + $1
     else
       @messages[e.userhost] = e.message if e.message?
     end
