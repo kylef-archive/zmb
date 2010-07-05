4b0e54bf43849c924d1d2a1b96fcb395afed0b3d
Kyle Fuller <inbox@kylefuller.co.uk>
2010-07-05 21:07:06 +0100

    Use our own API key for google translate

--- plugins/translate.rb
+++ plugins/translate.rb
@@ -15,7 +15,7 @@ class Translate
   def translate(e, to, from, message)
     request = JSON.parse("http://ajax.googleapis.com/ajax/services/language/translate".get({
       :v => '1.0',
-      :key => 'ABQIAAAAhwR5TtcQxY9fSuKy7yrBJhQ-sC4I4KvMQ8RG81t2M9sVc21w2xQUb9Dipx99m8XrHBsa3OctXe2rQw',
+      :key => 'ABQIAAAAtWAKkpCW6vL4tULVhqm_ZxQVlmwHIG1k2CM6GldPK9kOhyhAchSCYvJg4eFXVQuYYE7r4s1oNbga9A',
       :langpair => "#{to}%7C#{from}",
       :q => CGI.escape(message)
     }).body)
