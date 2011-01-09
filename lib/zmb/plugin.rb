cfccaa25dd143d73e0b153170cddc5f7eaa60219
Kyle Fuller <inbox@kylefuller.co.uk>
2011-01-09 22:53:47 +0000

    Create a PluginForwarder. This is usefull for classes which are not plugins

--- lib/zmb/plugin.rb
+++ lib/zmb/plugin.rb
@@ -26,12 +26,13 @@ class Plugin
     raise HaltCore.new(*args)
   end
 
-  def initialize(delegate, s)
+  def initialize(core, s)
+    @zmb = core
     @timers = Array.new
   end
 
   def plugins
-    @delegate.plugins
+    zmb.plugins
   end
 
   def debug(message, exception=nil)
@@ -63,5 +64,45 @@ class Plugin
 
   def del_timer(t)
     @timers.delete(t)
+  end
+end
+
+class PluginForwarder
+  # This class is useful for use in other classes a plugin might have.
+  # It allows you to use post, debug, halt, etc inside other
+  # non-plugin classes. It just requires @plugin to be set.
+
+  attr_accessor :plugin
+
+  def initialize(plug)
+    @plugin = plug
+  end
+
+  def halt(*args)
+    raise Halt.new(*args)
+  end
+
+  def haltcore(*args)
+    raise HaltCore.new(*args)
+  end
+  
+  def debug(message, exception=nil)
+    @plugin.debug(message, exception) if @plugin
+  end
+
+  def post(*args)
+    @plugin.post(*args) if @plugin
+  end
+
+  # Timers
+
+  def add_timer(symbol, interval, repeat=false, data=nil)
+    t = Timer.new(self, symbol, interval, repeat, data)
+    @plugin.timers << t
+    t
+  end
+
+  def del_timer(t)
+    @plugins.del_timer(t)
   end
 end
