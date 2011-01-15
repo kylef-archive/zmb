f18a1772e1cfa23aa9862c553f75e874852e8514
Kyle Fuller <inbox@kylefuller.co.uk>
2011-01-15 22:56:05 +0000

    Convert the quote plugin to work with the new zmb command api's

--- plugins/quote.rb
+++ plugins/quote.rb
@@ -3,95 +3,99 @@ class Quote <Plugin
   description "quote database"
 
   attr_accessor :quotes, :autoindex
-  
+
   def initialize(sender, s={})
+    super
     @quotes = Hash.new
     @quotes = s['quotes'] if s.has_key?('quotes')
     @autoindex = 1
     @autoindex = s['autoindex'] if s.has_key?('autoindex')
   end
-  
+
   def settings
     { 'quotes' => @quotes, 'autoindex' => @autoindex }
   end
-  
+
   def add(quote, username=nil)
     @quotes["#{@autoindex}"] = {
       'quote' => quote,
       'time' => Time.now,
       'username' => username,
     }
-    
+
     @autoindex += 1
     @autoindex - 1
   end
-  
+
   def count
     @quotes.keys.count
   end
-  
-  def commands
-    {
-      'quote' => [:quote_command, {
-        :help => 'Show a random quote or the quote with matching id',
-        :usage => 'id' }],
-      'quote-add' => [:add_command, {
-        :help => 'Add a quote',
-        :example => 'zynox: Hello!' }],
-      'quote-del' => [:del_command, { 
-        :help => 'Delete a quote by id',
-        :example => '7'}],
-      'quote-count' =>[lambda { |e| "#{count} quotes" }, 0, {
-        :help => 'Show amount of quotes' }],
-      'quote-last' => [:last_command, 0, { :help => 'Show the last quote' }],
-      'quote-search' => [:search_command, {
-        :help => 'Search to find a quote',
-        :usage => 'search' }],
-    }
-  end
-  
-  def quote_command(e, id=nil)
-    return "quote \##{id} not found" if id and not @quotes.has_key?(id)
-    return "\"#{@quotes[id]['quote']}\" by #{@quotes[id]['username']} at #{@quotes[id]['time']}" if id
-    return "no quotes" if count < 1
-    
-    id = "#{rand(autoindex - 1) + 1}"
-    while not @quotes.has_key?(id)
-      id = "#{rand(autoindex - 1) + 1}"
+
+  command :quote do
+    help 'Show a random quote or a quote matching the id supplied'
+    usage '[quote_id]'
+    regex /^(\d+)?$/
+
+    call do |m, quote_id|
+      if quote_id
+        return "Quote: #{id} not found" unless @quotes.has_key?(quote_id)
+      else
+        quote_id = "#{rand(autoindex - 1) + 1}"
+        while not @quotes.has_key?(quote_id)
+          quote_id = "#{rand(autoindex - 1) + 1}"
+        end
+      end
+
+      "\"#{@quotes[quote_id]['quote']}\" by #{@quotes[quote_id]['username']} at #{@quotes[quote_id]['time']}"
     end
-    
-    "\"#{@quotes[id]['quote']}\" by #{@quotes[id]['username']} at #{@quotes[id]['time']}"
   end
-  
-  def add_command(e, quote)
-    if e.user and e.user.respond_to?('authenticated?') and e.user.authenticated? then
-      "quote added \##{add(quote, e.user.username)}"
-    else
-      'permission denied'
+
+  command :quote_add do
+    help 'Add a quote'
+    usage 'quote' => '<kylef> Hello world!'
+    permission :authenticated
+    regex /^(.+)$/
+
+    call do |m, quote|
+      add(quote, m.opts[:user].username)
     end
   end
   
-  def del_command(e, id)
-    if @quotes.has_key?(id) then
-      @quotes.delete id
-      "quote #{id} deleted"
-    else
-      "no quote found with id=#{id}"
+  command :quote_rm do
+    help 'Remove a quote'
+    usage 'quote_id' => '5'
+    regex /^(\d+)?$/
+    permission :admin
+
+    call do |m, quote_id|
+      if @quotes.has_key?(quote_id)
+        @quotes.delete id
+        "Quote #{quote_id} has been deleted"
+      else
+        "No quotes found with the id=#{quote_id}"
+      end
     end
   end
-  
-  def last_command(e)
+
+  command :quote_last do
+    help 'Show the last quote added'
     return "no quotes" if count < 1
     quote_command(e, @quotes.keys.sort.reverse[0])
   end
-  
-  def search_command(e, search)
-    result = @quotes.map{ |id, quote| "#{id}: #{quote['quote']}" if quote['quote'].include?(search) }.reject{ |q| not q }
-    
-    if result.count then
-      result.join("\n")
-    else
-      "no quotes found"
+
+  command :quote_search do
+    help 'Search to find a quote'
+    regex /^(.+)$/
+
+    call do |m, search|
+      result = @quotes.map{ |id, quote| "#{id}: #{quote['quote']}" if quote['quote'].include?(search) }.reject{ |q| not q }
+
+      if result.count then
+        result.join("\n")
+      else
+        "no quotes found"
+      end
     end
   end
+end
 end
